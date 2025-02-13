50a5ff7d2ed51f1b4cbc1317851eaef9a7b50428
AlexisMadridB <jordimadrid52@gmail.com>
2025-02-12 19:33:20 -0500

    Commit 1

---
+++ Labo16/src/main/java/com/mycompany/labo16/Labo16.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.labo16;
+
+/**
+ *
+ * @author jordi
+ */
+public class Labo16 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
